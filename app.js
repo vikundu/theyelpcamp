5c078713ae54287d60e9c70d10b930434eed4e8b
Vikas Kundu <kundu.vikas13@gmail.com>
2018-02-18 15:07:39 +0000

    Adding models

--- app.js
+++ app.js
@@ -2,45 +2,12 @@ var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var seedDB = require("./seeds");
+
+seedDB();
 
 mongoose.connect("mongodb://localhost/yelp_camp");
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description:String
-});
-
-var Campground = mongoose.model("Campground",campgroundSchema);
-
-// Campground.create(
-//     {
-//         name:"Silver Creek", 
-//         image:"https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg",
-//         description:"One of the most beautiful site you'll see."
-//     },function(err,data)
-//     {
-//         if(err)
-//             console.log(err);
-//         else
-//         {
-//             console.log("Campground created..");
-//             console.log(data);
-//         }
-//     });
-
-
-// var campgrounds=[
-        
-//         {name:"Granite Hill", image:"https://static.pexels.com/photos/803226/pexels-photo-803226.jpeg"},
-//         {name:"Mountain Goat's Rest", image:"https://media1.fdncms.com/csindy/imager/u/original/5599678/summercamping-b3cb0e0608b3c56a.jpg"},
-//         {name:"Silver Creek", image:"https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg"},
-//         {name:"Granite Hill", image:"https://static.pexels.com/photos/803226/pexels-photo-803226.jpeg"},
-//         {name:"Mountain Goat's Rest", image:"https://media1.fdncms.com/csindy/imager/u/original/5599678/summercamping-b3cb0e0608b3c56a.jpg"},
-//         {name:"Silver Creek", image:"https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg"},
-//         {name:"Granite Hill", image:"https://static.pexels.com/photos/803226/pexels-photo-803226.jpeg"},
-//         {name:"Mountain Goat's Rest", image:"https://media1.fdncms.com/csindy/imager/u/original/5599678/summercamping-b3cb0e0608b3c56a.jpg"}
-//         ];
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -81,11 +48,11 @@ app.get("/campgrounds/new",function(req,res){
 
 app.get("/campgrounds/:id",function(req, res) {
     
-    Campground.findById(req.params.id,function(err,foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
        if(err){
            console.log(err)
        }else{
-           //console.log(foundCampground);
+           console.log(foundCampground);
            res.render("show",{campgrounds:foundCampground});
        }
         
